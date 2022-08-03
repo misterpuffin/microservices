791a1d938941e28167e52ff38758d635b8eeb316
misterpuffin <snghaoren@gmail.com>
2022-08-03 23:26:10 +0800

    Fix incorrect url

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -154,7 +154,7 @@ func (app *Config) sendMail(w http.ResponseWriter, msg MailPayload) {
 	jsonData, _ := json.MarshalIndent(msg, "", "\t")
 
 	// call the mail service
-	mailServiceURL := "http://mailer-service/send"
+	mailServiceURL := "http://mail-service/send"
 
 	// post to mail service
 	request, err := http.NewRequest("POST", mailServiceURL, bytes.NewBuffer(jsonData))

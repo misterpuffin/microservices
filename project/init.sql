27ace3f3f9d80f694aee56cb6aa9f06922a541fe
misterpuffin <snghaoren@gmail.com>
2022-08-03 23:08:16 +0800

    Fix init script for sql

--- project/init.sql
+++ project/init.sql
@@ -1,7 +1,3 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 --
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
@@ -56,4 +52,3 @@ ALTER TABLE ONLY public.users
 INSERT INTO "public"."users"("email","first_name","last_name","password","user_active","created_at","updated_at")
 VALUES
 (E'admin@example.com',E'Admin',E'User',E'$2a$12$1zGLuYDDNvATh4RA4avbKuheAMpb1svexSzrQm7up.bnpwQHs0jNe',1,E'2022-03-14 00:00:00',E'2022-03-14 00:00:00');
-EOSQL
